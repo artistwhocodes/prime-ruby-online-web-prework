c25bfcedb0ecf91354e10f96a3c0f2ac6f09ce8d
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-10 21:11:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,12 +2,23 @@
 
 
 def prime?(numbers)
-  if numbers <= 1
-    return false
-  elsif numbers <= 2
-    return true
-  else (2..numbers/2).none? do |x|
+  if (2..numbers/2).none? do |x|
     numbers % x == 0
+  elsif numbers <= 1
+    return false
+  else numbers <= 2
+    return true
   end
   end
 end
+
+# def prime?(numbers)
+#   if numbers <= 1
+#     return false
+#   elsif numbers <= 2
+#     return true
+#   else (2..numbers/2).none? do |x|
+#     numbers % x == 0
+#   end
+#   end
+# end
